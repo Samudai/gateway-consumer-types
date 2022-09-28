3f800106e2b7bfa8f00e76825d248d6b8fc44a61
yathishram <123yathish.r@gmail.com>
2022-09-28 12:10:25 +0400

    updates types

--- src/dashboard/types.ts
+++ src/dashboard/types.ts
@@ -1,60 +1,65 @@
+import { WidgetList } from './enums'
+
 export type Dashboard = {
   dashboard_id: string
   dao_id: string
   dashboard_name: string
   description: string
   default: boolean
+  visibility: string
 
   created_at?: string
   updated_at?: string
 }
 
-export type Widget = {
-  widget_id: number
-  widget_name: string
-  description: string
+// export type Widget = {
+//     widget_id: number;
+//     widget_name: string;
+//     description: string;
 
-  created_at?: string
-  updated_at?: string
-}
+//     created_at?: string;
+//     updated_at?: string;
+// };
 
 export type DashboardWidget = {
-  dashboard_widget_id: number
+  id: number
   dashboard_id: string
-  widget_id: number
+  type: WidgetList
   active: boolean
   row: number
   column: number
   order: number
   draggable: boolean
   popup_id: string
+  resizable: boolean
 
   created_at?: string
   updated_at?: string
 }
 
-export type DashboardView = {
+export type DashboardResponse = {
   dashboard_id: string
   dao_id: string
   dashboard_name: string
   description: string
   default: boolean
+  visibility: string
 
-  widgets: DashboardWidgetView[]
+  widgets: DashboardWidget[]
 
   created_at?: string
   updated_at?: string
 }
 
-export type DashboardWidgetView = {
-  dashboard_widget_id: number
-  dashboard_id: string
-  widget_id: number
-  active: boolean
-  row: number
-  column: number
-  order: number
-  draggable: boolean
-  popup_id: string
-  name: string
-}
+// export type DashboardWidgetView = {
+//     dashboard_widget_id: number;
+//     dashboard_id: string;
+//     widget_id: number;
+//     active: boolean;
+//     row: number;
+//     column: number;
+//     order: number;
+//     draggable: boolean;
+//     popup_id: string;
+//     name: string;
+// };
