06fd1b154751bac90a13859071cf15ee3e35f6a5
yathishram <123yathish.r@gmail.com>
2022-10-17 14:58:29 +0400

    adds notification enums

--- src/notifications/enums.ts
+++ src/notifications/enums.ts
@@ -25,3 +25,24 @@ export enum NotificationScope {
   PAYMENT = 0,
   REVIEWS = 1,
 }
+
+export enum SocketEventsToService {
+  'CONNECTION' = 'connection',
+  'DAO_CHANGE' = 'dao_change',
+  'PAYMENT_CREATED_NOTIFICATION' = 'payment_created_notification',
+  'CONTRIBUTOR_PAYMENT_RECEIVED' = 'contributor_payment_received',
+  'JOB_APPLICANT_SUBMITTED' = 'job_applicant_submitted',
+  'BOUNTY_SUBMISSION' = 'bounty_submission',
+  'TASK_REVIEW' = 'task_review',
+  'MEETING_CREATED' = 'meeting_created',
+  'SOCIAL_CONNECTION' = 'social_connection',
+  'PROJECT_ADDED' = 'project_added',
+  'COLLABORATION_REQUEST' = 'collaboration_request',
+  'DISCONNECT' = 'disconnect',
+}
+
+export enum SocketEventsFromService {
+  'MEMBER_CONNECTED' = 'memberConnected',
+  'PAYMENT_NOTIFICATION' = 'payment_notification',
+  'REVIEW_NOTIFICATION' = 'review_notification',
+}
