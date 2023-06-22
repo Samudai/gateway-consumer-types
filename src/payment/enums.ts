87e4290b2df744ca7546eaf1a53a8ec306ced094
VANSH KAPOOR <vanshkapoor2001@gmail.com>
2023-06-22 19:23:13 +0530

    version update

---
+++ src/payment/enums.ts
@@ -0,0 +1,11 @@
+export enum ProviderType {
+  'wallet' = 'wallet',
+  'gnosis' = 'gnosis',
+  'parcel' = 'parcel',
+}
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  FAILED = 'failed',
+}
---
+++ src/project/enums.ts
@@ -0,0 +1,38 @@
+export enum LinkType {
+  MEMBER = 'member',
+  CLAN = 'clan',
+  DAO = 'dao',
+}
+
+export enum ProjectType {
+  DEFAULT = 'default',
+  INTERNAL = 'internal',
+  INVESTMENT = 'investment',
+}
+
+export enum Visibility {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+}
+
+export enum TaskStatus {
+  TODO = 'to-do',
+  IN_PROGRESS = 'in-progress',
+  DONE = 'done',
+  IN_REVIEW = 'in-review',
+  BACKLOG = 'backlog',
+}
+
+export enum CommentType {
+  TASK = 'task',
+  PROJECT = 'project',
+}
+
+export enum ApplicantType {
+  MEMBER = 'member',
+  CALN = 'clan',
+}
+
+export enum ResponseType {
+  DEAL = 'deal',
+}
