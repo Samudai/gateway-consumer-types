85499954fd50c330f1f8976e7d6b799aa5aac174
RadientBrain <maheshwarichetan00@gmail.com>
2023-11-06 11:50:33 +0530

    updated onboarding progressbar

--- src/activity/enums.ts
+++ src/activity/enums.ts
@@ -54,18 +54,27 @@ export enum ActionType {
 }
 
 export enum DAOItems {
-  COMPLETE_PROFILE = 'complete_profile',
-  EXPLORE_DASHBOARD = 'explore_dashboard',
-  INVITE_MEMBERS = 'invite_members',
-  START_NEW_PROJECT = 'start_new_project',
-  POST_A_JOB = 'post_a_job',
-  COLLABORATION_PASS_CLAIM = 'collaboration_pass_claim',
+  SETUP_DAO_PROFILE = 'setup_dao_profile',
+  COMPLETE_INTEGRATIONS = 'complete_integrations',
+  CREATE_A_PROJECT = 'create_a_project',
+  // COMPLETE_PROFILE = 'complete_profile',
+  // EXPLORE_DASHBOARD = 'explore_dashboard',
+  // INVITE_MEMBERS = 'invite_members',
+  // START_NEW_PROJECT = 'start_new_project',
+  // POST_A_JOB = 'post_a_job',
+  // COLLABORATION_PASS_CLAIM = 'collaboration_pass_claim',
 }
 
 export enum ContributorItems {
-  COMPLETE_PROFILE = 'complete_profile',
-  INVITE_MEMBERS = 'invite_members',
-  CONNECT_WITH_CONTRIBUTORS = 'connect_with_contributors',
-  APPLY_FOR_JOB = 'apply_for_job',
-  NFT_CLAIM = 'nft_claim',
+  OPEN_TO_WORK = 'open_to_work',
+  ADD_TECHSTACK = 'add_techstack',
+  FEATURED_PROJECTS = 'featured_projects',
+  ADD_HOURLY_RATE = 'add_hourly_rate',
+  ACCEPT_PENDING_REQUESTS = 'accept_pending_requests',
+  CONNECT_TELEGRAM = 'connect_telegram',
+  // COMPLETE_PROFILE = 'complete_profile',
+  // INVITE_MEMBERS = 'invite_members',
+  // CONNECT_WITH_CONTRIBUTORS = 'connect_with_contributors',
+  // APPLY_FOR_JOB = 'apply_for_job',
+  // NFT_CLAIM = 'nft_claim',
 }
